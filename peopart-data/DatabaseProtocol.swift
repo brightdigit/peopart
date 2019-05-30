ccf9feb477921bf58afb37b54f743637b06cf21d
Leo Dion <leogdion@brightdigit.com>
2019-05-30 16:09:19 -0400

    added protocols and notes

---
+++ peopart-data/DatabaseProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  peopart-app
+//
+//  Created by Leo Dion on 5/30/19.
+//  Copyright © 2019 Leo Dion. All rights reserved.
+//
+
+import Foundation
+
+public protocol DatabaseProtocol {
+  static var shared : DatabaseProtocol { get }
+  func users (_ completion: @escaping (Result<[UserProtocol], Error>) -> Void)  
+}
---
+++ peopart-data/UserProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  UserProtocol.swift
+//  peopart-app
+//
+//  Created by Leo Dion on 5/30/19.
+//  Copyright © 2019 Leo Dion. All rights reserved.
+//
+
+import Foundation
+
+public protocol UserProtocol {
+  
+}
