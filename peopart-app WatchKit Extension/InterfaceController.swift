dea1bae2b0a14df1764643d79f3133a4fb54f500
Leo Dion <leogdion@brightdigit.com>
2019-05-30 13:21:59 -0400

    Initial Commit

---
+++ peopart-app WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  peopart-app WatchKit Extension
+//
+//  Created by Leo Dion on 5/30/19.
+//  Copyright © 2019 Leo Dion. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ peopart-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  peopart-app
+//
+//  Created by Leo Dion on 5/30/19.
+//  Copyright © 2019 Leo Dion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
