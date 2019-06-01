ecc00647a06808bc883093953c988a6fd72b31e1
Leo Dion <leogdion@brightdigit.com>
2019-05-31 20:34:37 -0400

    display menu with users and posts

--- peopart-app WatchKit Extension/InterfaceController.swift
+++ peopart-app WatchKit Extension/InterfaceController.swift
@@ -9,23 +9,56 @@
 import WatchKit
 import Foundation
 
+struct ItemDataType : DataType {
+  var numberOfItems: Int {
+    return 2
+  }
+  
+  func configure(_ rowController: RowController, forIndex index: Int) {
+    if index == 0 {
+      rowController.image.setImage(UIImage(named: "User"))
+      rowController.label.setText("Users")
+    } else {
+      rowController.image.setImage(UIImage(named: "Post"))
+      rowController.label.setText("Posts")
+    }
+  }
+   
+}
+
+protocol DataType {
+  var numberOfItems : Int { get }
+  func configure(_ rowController : RowController, forIndex index: Int)
+}
 
 class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+  var dataType : DataType = ItemDataType()
+  @IBOutlet weak var table: WKInterfaceTable!
+  
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
+    // Configure interface objects here.
+    table.setNumberOfRows(dataType.numberOfItems, withRowType: "row")
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    for index in 0..<dataType.numberOfItems {
+      guard let rowController = table.rowController(at: index) as? RowController else {
+        continue
+      }
+      dataType.configure(rowController, forIndex: index)
     }
-
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  
+  
 }
---
+++ peopart-app WatchKit Extension/RowController.swift
@@ -0,0 +1,14 @@
+//
+//  RowController.swift
+//  peopart-app
+//
+//  Created by Leo Dion on 5/31/19.
+//  Copyright © 2019 Leo Dion. All rights reserved.
+//
+
+import WatchKit
+
+class RowController: NSObject {
+  @IBOutlet weak var image: WKInterfaceImage!
+  @IBOutlet weak var label: WKInterfaceLabel!
+}
