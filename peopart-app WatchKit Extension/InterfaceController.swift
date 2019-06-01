a6ba07f9230e5d9a4958e83cf42f146b0cdc58aa
Leo Dion <leogdion@brightdigit.com>
2019-06-01 09:06:32 -0400

    working on watch app

--- peopart-app WatchKit Extension/InterfaceController.swift
+++ peopart-app WatchKit Extension/InterfaceController.swift
@@ -10,42 +10,114 @@ import WatchKit
 import Foundation
 
 struct ItemDataType : DataType {
-  var numberOfItems: Int {
-    return 2
+  func fetch(_ completion: ([DataItem]) -> Void) {
+    completion( [ItemType(type: .user), ItemType(type: .post)] )
   }
   
-  func configure(_ rowController: RowController, forIndex index: Int) {
-    if index == 0 {
-      rowController.image.setImage(UIImage(named: "User"))
-      rowController.label.setText("Users")
-    } else {
-      rowController.image.setImage(UIImage(named: "Post"))
-      rowController.label.setText("Posts")
-    }
+//  var numberOfItems: Int {
+//    return 2
+//  }
+//
+//  func configure(_ rowController: RowController, forIndex index: Int) {
+
+//  }
+  
+}
+
+class ItemType : NSObject, DataItem {
+  
+  let type : Int
+  let dataType: DataType
+  
+  init (type: Types) {
+    self.type = type.rawValue
+    self.dataType = ItemDataType()
   }
-   
+  enum Types : Int {
+    case user = 0, post = 1
+  }
+  func controller(_ controller: WKInterfaceController, table: WKInterfaceTable, configureRowAtIndex index: Int) {
+        guard let type = Types(rawValue: index) else {
+          return
+        }
+        guard let rowController = table.rowController(at: index) as? RowController else {
+          return
+        }
+        switch (type) {
+        case .user:
+          rowController.image.setImage(UIImage(named: "User"))
+          rowController.label.setText("Users")
+        case .post:
+          rowController.image.setImage(UIImage(named: "Post"))
+          rowController.label.setText("Posts")
+        }
+  }
+  
+  func controller(_ controller: WKInterfaceController, table: WKInterfaceTable, didSelectRowAt index: Int) {
+    controller.pushController(withName: "table", context: self)
+  }
+  
+//  func table(_ table: WKInterfaceTable, configureRowAtIndex index: Int) {
+
+//  }
+  
+  
+}
+
+protocol DataItem {
+  func controller( _ controller : WKInterfaceController, table: WKInterfaceTable, configureRowAtIndex index: Int)
+  func controller( _ controller : WKInterfaceController, table: WKInterfaceTable, didSelectRowAt index: Int)
+  
+  var dataType : DataType { get }
 }
 
 protocol DataType {
-  var numberOfItems : Int { get }
-  func configure(_ rowController : RowController, forIndex index: Int)
+  //var numberOfItems : Int { get }
+  //func configure(_ rowController : RowController, forIndex index: Int)
+ 
+  func fetch (_ completion :  @escaping ([DataItem]) -> Void )
 }
 
 class InterfaceController: WKInterfaceController {
   var dataType : DataType = ItemDataType()
+  var items : [DataItem]? = nil
   @IBOutlet weak var table: WKInterfaceTable!
   
   override func awake(withContext context: Any?) {
     super.awake(withContext: context)
     
-    // Configure interface objects here.
-    table.setNumberOfRows(dataType.numberOfItems, withRowType: "row")
+    if let itemType = context as? DataItem {
+      self.dataType = itemType.dataType
+    }
     
-    for index in 0..<dataType.numberOfItems {
-      guard let rowController = table.rowController(at: index) as? RowController else {
-        continue
+    
+    // Configure interface objects here.
+    dataType.fetch { (items) in
+      self.items = items
+      DispatchQueue.main.async {
+        self.reload()
       }
-      dataType.configure(rowController, forIndex: index)
+      
+    }
+    
+//    table.setNumberOfRows(dataType.numberOfItems, withRowType: "row")
+//
+//    for index in 0..<dataType.numberOfItems {
+//      guard let rowController = table.rowController(at: index) as? RowController else {
+//        continue
+//      }
+//      dataType.configure(rowController, forIndex: index)
+//    }
+  }
+  
+  func reload () {
+    guard let items = self.items else {
+      return
+    }
+    table.setNumberOfRows(items.count, withRowType: "row")
+    
+    for (index, item) in items.enumerated() {
+      item.controller(self, table: table, configureRowAtIndex: index)
     }
   }
   
@@ -60,5 +132,12 @@ class InterfaceController: WKInterfaceController {
   }
   
   
+  override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+    guard let item = items?[rowIndex] else {
+      return
+    }
+    
+    item.controller(self, table : table, didSelectRowAt: rowIndex)
+  }
   
 }
