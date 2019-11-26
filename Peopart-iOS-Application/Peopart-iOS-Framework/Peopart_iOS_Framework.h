7806114143395707a9f2f7648f709232350e36cc
Leo Dion <leogdion@brightdigit.com>
2019-11-26 14:35:55 -0500

    working xcode project

---
+++ Peopart-iOS-Application/Peopart-iOS-Framework/Peopart_iOS_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Peopart_iOS_Framework.h
+//  Peopart-iOS-Framework
+//
+//  Created by Leo Dion on 11/26/19.
+//  Copyright © 2019 BrightDigit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Peopart_iOS_Framework.
+FOUNDATION_EXPORT double Peopart_iOS_FrameworkVersionNumber;
+
+//! Project version string for Peopart_iOS_Framework.
+FOUNDATION_EXPORT const unsigned char Peopart_iOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Peopart_iOS_Framework/PublicHeader.h>
+
+
