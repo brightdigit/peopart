ece07cbf546a5c76d622eba9a811376e4cf1c556
Leo Dion <leogdion@brightdigit.com>
2019-11-26 14:23:56 -0500

    working on issue with framework in xcodegen

--- Application/iOS/main.swift
+++ Application/iOS/main.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Peopart_iOS_framework
 
 UIApplicationMain(
     CommandLine.argc,
--- Sources/iOS/Peopart.swift
+++ Sources/iOS/Peopart.swift
@@ -1,8 +1,33 @@
 import UIKit
+import SwiftUI
 
-class AppDelegate : UIResponder, UIApplicationDelegate {
+struct ContentView : View {
+  var body: some View {
+    Text("Hello World")
+  }
 }
 
-class PeopartApplication : UIApplication {
+class SceneDelegate : UIResponder, UIWindowSceneDelegate {
+  var window : UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    if let windowScene = scene as? UIWindowScene {
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: ContentView())
+      self.window = window
+      window.makeKeyAndVisible()
+    }
+  }
+}
+
+public class AppDelegate : UIResponder, UIApplicationDelegate {
+  public func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    var configuration = UISceneConfiguration()
+    configuration.delegateClass = SceneDelegate.self
+    return configuration
+  }
+}
+
+public class PeopartApplication : UIApplication {
   
 }
