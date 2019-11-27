ac23e3a2ceaced0c52fa497abfe54b1608bc86a3
Leo Dion <leogdion@brightdigit.com>
2019-11-27 04:22:28 -0500

    fixed screen size

--- Sources/iOS/Peopart.swift
+++ Sources/iOS/Peopart.swift
@@ -21,10 +21,17 @@ class SceneDelegate : UIResponder, UIWindowSceneDelegate {
 }
 
 public class AppDelegate : UIResponder, UIApplicationDelegate {
+  public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
   public func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    let configuration = UISceneConfiguration()
+    let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     configuration.delegateClass = SceneDelegate.self
     return configuration
+  }
+  
+  public func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    
   }
 }
 
