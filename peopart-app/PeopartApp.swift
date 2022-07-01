a4dc8993dc824fae028e776dee0e07d1e3955c92
Leo Dion <leogdion@brightdigit.com>
2022-07-01 10:51:55 -0400

    working on refactoring

---
+++ peopart-app/PeopartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopartApp.swift
+//  peopart-app
+//
+//  Created by Leo Dion on 7/1/22.
+//  Copyright © 2022 Leo Dion. All rights reserved.
+//
+
+import SwiftUI
+
+struct PeopartApp : App {
+  var body: some Scene {
+    WindowGroup {
+      UserListView()
+    }
+  }
+}
---
+++ peopart-app/UserListView.swift
@@ -0,0 +1,21 @@
+//
+//  UserListView.swift
+//  peopart-app
+//
+//  Created by Leo Dion on 7/1/22.
+//  Copyright © 2022 Leo Dion. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserListView()
+    }
+}
